fa81ef47b07dccde0e2dbb216cde35336658d3e7
Chris Billows <christopherbillows@gmail.com>
2023-04-16 11:28:23 +0100

    Add working readchar key press  methods to test

--- testing.py
+++ testing.py
@@ -1,71 +1,45 @@
-import time
-from my_funcs import *
-import sys
-import tty
-import termios
-import select
-import os
-from playsound import playsound
+import readchar
+from time import sleep
+
+def press_x_or_o():
+    print("SELECT o OR x")
+    user_symbol = None
+
+    while True:
+        key = readchar.readkey().lower()
+        if key == "o" or key == "x":
+            user_symbol = key
+            break
+        else:
+            print("INVALID INPUT. PLEASE SELECT o OR x")
+
+    print(f"GREAT. YOU HAVE SELECTED {user_symbol.upper()}")
+
+def press_any_key():
+    print("\n\nOkay, LET'S GO!\n\nPress any key to start the game!")
+    key = readchar.readkey().lower()
+    # while key == '':
+    #     print("not pressed yet")
+    #     sleep(0.2)    
+    return type(key)
+
+    # while True:
+    #     key = readchar.readkey().lower()
+    #     if key == "o" or key == "x":
+    #         user_symbol = key
+    #         break
+    #     else:
+    #         print("INVALID INPUT. PLEASE SELECT o OR x")
+
+    # print(f"GREAT. YOU HAVE SELECTED {user_symbol.upper()}")
+
+# my_str = press_any_key()
+
+# if my_str != '':
+#     print("STRING")
+# else:
+#     print("TWAT")
+print(press_any_key())
 
 
 
-                
-
-def print_win():
-    end_time = time.time() + 3 # 5 seconds from now
-    playsound("sounds/win-sfx-38507.mp3")
-    while time.time() < end_time:
-        print("YOU WIN! ", end=" ", flush=True)
-        time.sleep(0.009) 
-                                       
-def print_win_keyboard_interupt():
-    end_time = time.time() + 10 # 5 seconds from now
-    old_settings = termios.tcgetattr(sys.stdin) # get the current terminal settings
-    tty.setcbreak(sys.stdin.fileno()) # set the terminal to cbreak mode
-    try:
-        while time.time() < end_time:
-            print("YOU WIN! ", end=" ", flush=True)
-            time.sleep(0.009) # delay to slow down printing
-            if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
-                break # terminate the loop if any key is pressed
-    finally:
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings) # restore the original terminal settings
-
-def play_sound():
-    print(winner_msg)
-    print(winner_face)
-    playsound("sounds/win-sfx-38507.mp3", block=True)
-
-
-
-active_player = ('user', 'o')
-active_player2 = ('computer', 'o')
-
-
-def winner_symbol_animation(active_player):
-    if active_player[1] == 'x':
-        winner_ascii = x_winner
-    else:
-        winner_ascii = o_winner
-
-    count = 3
-    while count > 0:
-        print(winner_ascii, end="", flush=True)
-        os.system('afplay /System/Library/Sounds/Submarine.aiff')
-        count -=1
-    print(f'{"*" * 21}\n** Three in a row! **\n{"*" * 21}\n\n\n') 
-
-
-def end_game_msg(active_player):
-    if active_player[0] == 'user':
-        print(user_winner_msg)
-        print(user_winner_face)
-        playsound("sounds/win-sfx-38507.mp3")
-    else:
-        print(user_loser_msg)
-        print(user_loser_face)
-        playsound("sounds/game-over-38511.mp3")
-        
- 
-print(winner_symbol_animation(active_player))
-print(end_game_msg(active_player))
