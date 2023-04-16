f2c27b34f7ff6481d81afa9874e59bbebb4da005
Chris Billows <christopherbillows@gmail.com>
2023-04-16 12:03:43 +0100

    Add readchar revised decide_symbols and wait_for_x_o to my_funcs

--- my_funcs.py
+++ my_funcs.py
@@ -24,14 +24,28 @@ def wait_for_any_key():
     readchar.readkey()
 
 
+def wait_for_x_or_o():
+    while True:
+        key = readchar.readkey().lower()
+        if key == "o" or key == "x":
+            user_symbol = key
+            return key
+        else:
+            print("\n------INVALID INPUT------\n\nPlease press either'o' or 'x': ")
+  
+
 def decide_symbols():
     symbols = ['x', 'o']
-    user_symbol =  input("Do you want to be noughts or crosses (o or x): ")
+    print("Do you want to be noughts or crosses (o or x): ")
+    user_symbol = wait_for_x_or_o()
+        
     symbols.remove(user_symbol)
     computer_symbol = symbols[0]
+
     print("\n", 46 * "-")
     print(f"\nGreat! You will play as {user_symbol}'s.")
     print(f"\nI will play as {computer_symbol}'s.\n")
+
     return user_symbol, computer_symbol
 
 
