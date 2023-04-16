7a7cd02fea0b1ec1991ad9b19220ac126210b0a1
Chris Billows <christopherbillows@gmail.com>
2023-04-16 14:44:07 +0100

    Add coin toss and start SFX to app.py

--- my_funcs.py
+++ my_funcs.py
@@ -59,18 +59,39 @@ def decide_symbols():
   
     return user_symbol, computer_symbol
 
+def coin_toss():
+    for i in range(15):
+        sleep(0.2)
+        coin_toss = choice([' H ', ' T '])
+        print(coin_toss, end='', flush=True)
+        # os.system('afplay /System/Library/Sounds/Ping.aiff')
+    sleep(0.01)
+    print()
+    return coin_toss
+
 
 def randomise_first_to_play(user_symbol, computer_symbol):
-    print("\nPress any key to randomise goes first...\n\n" + 46 *"-")
-    computer_thinking_animation()
+    coin_dict = {'h':'heads', 't':'tails'}
+    print("\n", 46 * "-")
+    print("\n\nLets see who goes first!\n\nPick h for heads or t for tails: " )  # ...\n\n" + 46 *"-")
+    user_coin = coin_dict[wait_for_h_or_t()]
+    print(f"\n\nYou selected {user_coin.upper()}. \n\nGOOD LUCK!\n")
+       
+    coin_winner = coin_dict[coin_toss().strip().lower()]
 
-    players = ['user', 'computer']
-    player1 = choice(players)
-    players.remove(player1)
+    if user_coin == coin_winner:
+        print(f"\nYES! {user_coin} wins!")
+        player1 = 'user'
+        player2 = 'computer'
+    else:
+        print(f"\nOh no! {user_coin} loses!")
+        player1 = 'computer'
+        player2 = 'user'                
 
-    print(f"\nGoing first will be:  {player1.upper()}")
-    print(f"Going second will be:  {''.join(players).upper()}")
+    print(f"\nSo going first will be:  {player1.upper()}")
+    print(f"And going second will be:  {player2.upper()}")
     os.system('afplay /System/Library/Sounds/Ping.aiff')
+    
     sleep(1)
     
     if player1 == 'user':
@@ -82,6 +103,7 @@ def randomise_first_to_play(user_symbol, computer_symbol):
 def game_start():
     print("\n\nOkay, LET'S GO!\n\nPress any key to start the game!")
     wait_for_any_key()
+    playsound("sounds/321-go.mp3")
 
 
 
